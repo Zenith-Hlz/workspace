e8a3de043a4cf6e1f3abdd527eb8575a8f7efae0
Zenith <houlinzhi23411082@gmail.com>
2024-12-09 13:25:41 +0800

    set workspace for PA4 game, adding test.cpp and gen.cpp for pai, and modify compare.sh accordingly

--- compare.sh
+++ compare.sh
@@ -6,42 +6,46 @@ read -p "Enter the number of times to run the script: " num_runs
 for ((i=1; i<=num_runs; i++)); do
     echo "Run #$i"
 
-    # Step 1: Run the Python file `gen.py` to generate input and store it in `./input/input.txt`
-    python3 gen.py > ./input/input.txt
+    # Step 1: Compile and run the C++ file `gen.cpp` to generate input and store it in `./input/input.txt`
+    g++ -o gen gen.cpp
     if [ $? -ne 0 ]; then
-        echo "Error: Failed to run gen.py on run #$i"
+        echo "Error: Compilation of gen.cpp failed on run #$i"
         exit 1
     fi
 
-    # Step 2: Compile `test.cpp`
+    ./gen > ./input/input.txt
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to run gen.cpp on run #$i"
+        exit 1
+    fi
+
+    # Step 2: Compile and run `test.cpp` using `./input/input.txt` and save output to `./output/output_test.txt`
     g++ -o test test.cpp
     if [ $? -ne 0 ]; then
         echo "Error: Compilation of test.cpp failed on run #$i"
         exit 1
     fi
 
-    # Step 3: Run `test` using `./input/input.txt` and save output to `./output/output_main.txt`
     ./test < ./input/input.txt > ./output/output_test.txt
     if [ $? -ne 0 ]; then
         echo "Error: test.cpp encountered a runtime error on run #$i"
         exit 1
     fi
 
-    # Step 4: Compile `main.cpp`
+    # Step 3: Compile and run `main.cpp` using `./input/input.txt` and save output to `./output/output_main.txt`
     g++ -o main main.cpp
     if [ $? -ne 0 ]; then
         echo "Error: Compilation of main.cpp failed on run #$i"
         exit 1
     fi
 
-    # Step 5: Run `main` using `./input/input.txt` and save output to `./output/output_main.txt`
     ./main < ./input/input.txt > ./output/output_main.txt
     if [ $? -ne 0 ]; then
         echo "Error: main.cpp encountered a runtime error on run #$i"
         exit 1
     fi
 
-    # Step 6: Compare outputs
+    # Step 4: Compare outputs
     if ! cmp -s ./output/output_test.txt ./output/output_main.txt; then
         echo "Error: Outputs are different on run #$i"
         exit 1
--- gen.cpp
+++ gen.cpp
@@ -1,161 +1,49 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <cstdlib>
 #include <ctime>
+#include <vector>
 #include <unordered_set>
-#include <algorithm>
-
 using namespace std;
 
-// Structure for a data item
-struct DataItem
+struct PairHash
 {
-    string key;
-    int value;
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2> &p) const
+    {
+        auto hash1 = std::hash<T1>{}(p.first);
+        auto hash2 = std::hash<T2>{}(p.second);
+
+        // Combining hashes using a simple hash combine technique
+        return hash1 ^ hash2;
+    }
 };
 
-// Function to read data from the file
-vector<DataItem> readDataFromFile(const string &filename)
+int n = 100000;
+int m = 100000;
+
+int main()
 {
-    vector<DataItem> data;
+    srand(static_cast<unsigned int>(time(0)));
+    cout << n << " " << m << endl;
 
-    ifstream infile(filename);
-    if (!infile)
-    {
-        cerr << "Error: Cannot open file " << filename << endl;
-        exit(1);
-    }
+    // Generate random weights for nodes
+    for (int i = 0; i < n; i++)
+        cout << rand() % 1001 << ' ';
+    cout << endl;
 
-    string line;
-    while (getline(infile, line))
+    // Generate random edges
+    unordered_set<pair<int, int>, PairHash> edges; // Use the custom hash function
+    for (int i = 0; i < m;)
     {
-        size_t pos1 = line.find(' ');
-        size_t pos2 = line.find(' ', pos1 + 1);
-        if (pos1 != string::npos && pos2 != string::npos)
+        int a = rand() % n + 1; // Random node 1 to n
+        int b = rand() % n + 1; // Random node 1 to n
+
+        // Ensure a != b (no self-loops) and the edge (a, b) is not a duplicate
+        if (a != b && edges.insert({min(a, b), max(a, b)}).second)
         {
-            string key = line.substr(pos1 + 1, pos2 - pos1 - 1);
-            int value = stoi(line.substr(pos2 + 1));
-            data.push_back({key, value});
+            cout << a << " " << b << endl;
+            i++;
         }
     }
-
-    infile.close();
-    return data;
-}
-
-// Function to generate uniform distribution test data
-vector<string> generateUniformTestData(const vector<DataItem> &data, int insertOps, int queryOps, unordered_set<string> &insertedKeys)
-{
-    int dataSize = data.size();
-    vector<string> output; // Vector to store generated input
-
-    // Insert operations
-    for (int i = 0; i < insertOps && i < dataSize; ++i)
-    {
-        int index = rand() % dataSize;
-        while (insertedKeys.count(data[index].key))
-        { // Avoid duplicate inserts
-            index = rand() % dataSize;
-        }
-        insertedKeys.insert(data[index].key);
-        output.push_back("0 " + data[index].key + " " + to_string(data[index].value));
-    }
-
-    // Query operations
-    for (int i = 0; i < queryOps; ++i)
-    {
-        if (rand() % 2 == 0 && !insertedKeys.empty())
-        { // Query inserted keys
-            auto it = insertedKeys.begin();
-            advance(it, rand() % insertedKeys.size());
-            output.push_back("1 " + *it);
-        }
-        else
-        { // Query random or non-inserted keys
-            int index = rand() % dataSize;
-            output.push_back("1 " + data[index].key);
-        }
-    }
-
-    return output;
-}
-
-vector<string> generateRandomTestData(const vector<DataItem> &data, int insertOps, int queryOps, unordered_set<string> &insertedKeys)
-{
-    vector<string> output = generateUniformTestData(data, insertOps, queryOps, insertedKeys);
-
-    random_shuffle(output.begin(), output.end());
-
-    return output;
-}
-
-// Function to print test data
-void printTestData(const vector<string> &testData)
-{
-    for (const auto &line : testData)
-        cout << line << endl;
-    cout << "2" << endl; // End of operations
-}
-
-// Main function to generate test data
-void generateTestData(const vector<DataItem> &data, int insertCount, int queryCount, const string &distribution)
-{
-    unordered_set<string> insertedKeys;
-    int insertOps = insertCount;
-    int queryOps = queryCount;
-
-    vector<string> testData;
-
-    if (distribution == "uniform")
-        testData = generateUniformTestData(data, insertOps, queryOps, insertedKeys);
-    else if (distribution == "random")
-        testData = generateRandomTestData(data, insertOps, queryOps, insertedKeys);
-    else
-    {
-        cerr << "Error: Unknown distribution type " << distribution << endl;
-        exit(1);
-    }
-
-    printTestData(testData);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 6)
-    {
-        cerr << "Usage: " << argv[0] << " <input_file> <insert_count> <query_count> <distribution> <output_file>" << endl;
-        cerr << "Distribution types: uniform, random" << endl;
-        return 1;
-    }
-
-    string inputFile = argv[1];
-    int insertCount = stoi(argv[2]);
-    int queryCount = stoi(argv[3]);
-    string distribution = argv[4];
-    string outputFile = argv[5];
-
-    vector<DataItem> data = readDataFromFile(inputFile);
-
-    // Redirect output to a file
-    ofstream outfile(outputFile);
-    if (!outfile)
-    {
-        cerr << "Error: Cannot open output file " << outputFile << endl;
-        return 1;
-    }
-
-    // Redirect standard output to the file
-    streambuf *coutbuf = cout.rdbuf();
-    cout.rdbuf(outfile.rdbuf());
-
-    generateTestData(data, insertCount, queryCount, distribution);
-
-    // Restore standard output
-    cout.rdbuf(coutbuf);
-
-    cout << "Test data generated in " << outputFile << endl;
 
     return 0;
 }
